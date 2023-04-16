8ac90346728cd4b08861781237ff9988af27f01b
West <agus2kcoin@outlook.com>
2023-04-16 20:27:53 -0300

    Add text object clones

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import './main.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
-import fragment from './shaders/fragment.glsl.js';
-import vertex from './shaders/vertex.glsl.js';
-import importsVertex from './shaders/imports.glsl.js';
-import beginVertex from './shaders/beginVertex.glsl.js';
-import beginNormalVertex from './shaders/beginNormalVertex.glsl';
 import font from './fonts/folklore_regular.data';
+import importsVertex from './shaders/vertex/imports.glsl.js';
+import beginVertex from './shaders/vertex/beginVertex.glsl.js';
+import beginNormalVertex from './shaders/vertex/beginNormalVertex.glsl';
+import importsFragment from './shaders/fragment/imports.glsl';
+import outputFragment from './shaders/fragment/outputFragment.glsl';
 
 export default class Sketch {
   constructor() {
@@ -44,7 +44,7 @@ export default class Sketch {
     //   1000
     // );
 
-    const frustrumSize = 1.5;
+    const frustrumSize = 0.75;
     const aspect = this.width / this.height;
     this.camera = new THREE.OrthographicCamera(
       (frustrumSize * aspect) / -2,
@@ -110,15 +110,64 @@ export default class Sketch {
     this.camera.updateProjectionMatrix();
   }
 
+  getMaterials(uniforms) {
+    const material = new THREE.MeshStandardMaterial({
+      color: 0x333333,
+    });
+
+    material.onBeforeCompile = (shader) => {
+      material.userData.shader = shader;
+      // Uniforms
+      shader.uniforms.uTime = uniforms.uTime;
+      shader.uniforms.uMin = uniforms.uMin;
+      shader.uniforms.uMax = uniforms.uMax;
+      shader.uniforms.uOffset = uniforms.uOffset;
+
+      // Fragment shader
+      shader.fragmentShader = importsFragment + shader.fragmentShader;
+
+      // output_fragment
+      const outputFragmentString = /* glsl */ `#include <output_fragment>`;
+      shader.fragmentShader = shader.fragmentShader.replace(
+        outputFragmentString,
+        outputFragmentString + outputFragment
+      );
+
+      // Vertex shader
+      shader.vertexShader = importsVertex + shader.vertexShader;
+
+      // beginnormal_vertex
+      const beginNormalVertexString = /* glsl */ `#include <beginnormal_vertex>`;
+      shader.vertexShader = shader.vertexShader.replace(
+        beginNormalVertexString,
+        beginNormalVertexString + beginNormalVertex
+      );
+
+      // begin_vertex
+      const beginVertexString = /* glsl */ `#include <begin_vertex>`;
+      shader.vertexShader = shader.vertexShader.replace(
+        beginVertexString,
+        beginVertexString + beginVertex
+      );
+    };
+
+    return material;
+  }
+
   addLights() {
-    const ambLight = new THREE.AmbientLight(0xffffff, 0.9); // soft white light
+    const ambLight = new THREE.AmbientLight(0xffff00, 1.1); // soft white light
     this.scene.add(ambLight);
     this.scene2.add(ambLight.clone());
 
-    const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    const dirLight = new THREE.DirectionalLight(0x00ff00, 0.3);
     dirLight.position.set(0, -1, 0);
     this.scene.add(dirLight);
     this.scene2.add(dirLight.clone());
+
+    const dirLight1 = new THREE.DirectionalLight(0xffffff, 0.9);
+    dirLight1.position.set(1, 1, 0);
+    this.scene.add(dirLight1);
+    this.scene2.add(dirLight1.clone());
   }
 
   addMesh() {
@@ -127,40 +176,24 @@ export default class Sketch {
       resolution: { value: new THREE.Vector4() },
       uMin: { value: new THREE.Vector3(0, 0, 0) },
       uMax: { value: new THREE.Vector3(0, 0, 0) },
+      uOffset: { value: 0 },
     };
 
-    this.material = new THREE.MeshStandardMaterial({
-      color: 0x333333,
-    });
-
-    this.material.onBeforeCompile = (shader) => {
-      this.material.userData.shader = shader;
-      // Uniforms
-      shader.uniforms.uTime = this.uniforms.uTime;
-      shader.uniforms.uMin = this.uniforms.uMin;
-      shader.uniforms.uMax = this.uniforms.uMax;
-
-      // Vertex shader
-      shader.vertexShader = importsVertex + shader.vertexShader;
-
-      // Make rounded
-      const beginVertexString = /* glsl */ `#include <begin_vertex>`;
-      shader.vertexShader = shader.vertexShader.replace(
-        beginVertexString,
-        beginVertexString + beginVertex
-      );
-
-      const beginNormalVertexString = /* glsl */ `#include <beginnormal_vertex>`;
-      shader.vertexShader = shader.vertexShader.replace(
-        beginNormalVertexString,
-        beginNormalVertexString + beginNormalVertex
-      );
+    this.uniforms1 = {
+      uTime: { value: 0 },
+      resolution: { value: new THREE.Vector4() },
+      uMin: { value: new THREE.Vector3(0, 0, 0) },
+      uMax: { value: new THREE.Vector3(0, 0, 0) },
+      uOffset: { value: 1 },
     };
+
+    this.material = this.getMaterials(this.uniforms);
+    this.material1 = this.getMaterials(this.uniforms1);
 
     const loader = new FontLoader();
 
     loader.load(font, (font) => {
-      const geometry = new TextGeometry('Euphoria', {
+      const geometry = new TextGeometry('euphoria', {
         font: font,
         size: 0.1,
         height: 0.1,
@@ -168,36 +201,60 @@ export default class Sketch {
         bevelEnabled: false,
       });
       // geometry = new THREE.BoxGeometry(0.5, 0.1, 0.1, 100, 100, 100);
+
+      // Clones
       let dummy = new THREE.BoxGeometry(0.15, 0.00001, 0.0001).toNonIndexed();
       let clone = geometry.clone();
 
       clone.computeBoundingBox();
       dummy.translate(clone.boundingBox.max.x, 0, 0);
 
-      let final = mergeGeometries([dummy, clone]);
+      const finalText = mergeGeometries([dummy, clone]);
 
       geometry.center();
       geometry.computeBoundingBox();
 
-      const bBox = geometry.boundingBox;
+      const finalClone = geometry.clone();
+      finalClone.computeBoundingBox();
+
+      const clonesArray = [];
+
+      for (let i = 0; i < 4; i++) {
+        const clone = finalClone.clone();
+        clone.center();
+        clone.translate(finalClone.boundingBox.max.x * i * 2, 0, 0);
+        clone.rotateX((Math.PI / 2) * i);
+        clonesArray.push(clone);
+      }
+
+      const finalTextClones = mergeGeometries([...clonesArray]);
+      finalTextClones.center();
+      finalTextClones.computeBoundingBox();
+
+      // Max/Min values
+      const bBox = finalTextClones.boundingBox;
       this.uniforms.uMin.value.x = bBox.min.x;
       this.uniforms.uMax.value.x = bBox.max.x;
 
+      this.uniforms1.uMin.value.x = bBox.min.x;
+      this.uniforms1.uMax.value.x = bBox.max.x;
+
       // Mesh
-      const mesh = new THREE.Mesh(geometry, this.material);
-      const mesh2 = new THREE.Mesh(geometry, this.material);
+      const mesh = new THREE.Mesh(finalTextClones, this.material);
+      const mesh2 = new THREE.Mesh(finalTextClones, this.material1);
       this.group.add(mesh);
       this.group2.add(mesh2);
 
       // Axes helper
       const axesHelper = new THREE.AxesHelper(5);
-      this.scene.add(axesHelper);
+      // this.scene.add(axesHelper);
     });
   }
 
   render() {
-    this.time += 0.05;
+    this.time += 0.02;
     this.uniforms.uTime.value = this.time;
+    this.uniforms1.uTime.value = this.time;
     window.requestAnimationFrame(this.render.bind(this));
 
     // Scenes
@@ -206,6 +263,10 @@ export default class Sketch {
 
     this.renderer.setScissor(this.width / 2, 0, this.width, this.height);
     this.renderer.render(this.scene2, this.camera);
+
+    // Test
+    // this.renderer.setScissor(0, 0, this.width, this.height);
+    // this.renderer.render(this.scene, this.camera);
   }
 }
 
