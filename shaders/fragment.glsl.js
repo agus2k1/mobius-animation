47e58aae87787af62ea17e602949c97365301bbf
West <agus2kcoin@outlook.com>
2023-04-16 00:09:11 -0300

    Add fragment shader

---
+++ shaders/fragment.glsl.js
@@ -0,0 +1,11 @@
+const fragmentShader = /* glsl */ `
+    varying vec2 vUv;
+    varying float vDebug;
+
+    void main() {
+        // gl_FragColor = vec4( vDebug, 0., 0., 1.);
+        gl_FragColor = vec4( vUv, 0., 1.);
+    }
+`;
+
+export default fragmentShader;
